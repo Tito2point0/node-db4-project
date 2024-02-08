81121faca6645c9af856dfc255792a560b544ce1
Tito2point0 <defconx00@gmail.com>
2024-02-07 21:50:36 -0500

    wip initial migrations done

--- migrations/20240208021745_initial-migration.js
+++ migrations/20240208021745_initial-migration.js
@@ -14,10 +14,28 @@ exports.up = async function(knex) {
             table.string('ingredient_unit', 50).notNullable()
         })
         .createTable('steps', table => {
-            table.increments()
+            table.increments('step_id')
+            table.string('step_text', 200).notNullable()
+            table.integer('step_number').notNullable()
+            table.integer('recipe_id')
+                .unsigned()
+                .notNullable()
+                .references('recipe_id')
+                .inTable('recipes')
+                .onUpdate('RESTRICT')
+                .onDelete('RESTRICT')
+
         })
         .createTable('step_ingredients', table => {
-            table.increments()
+            table.increments('step_ingredient_id')
+            table.float('quantity').notNullable()
+            table.integer('step_id')
+                .unsigned()
+                .notNullable()
+                .references('step_id')
+                .inTable('steps')
+                .onUpdate('RESTRICT')
+                .onDelete('RESTRICT')
         })
     };
 
